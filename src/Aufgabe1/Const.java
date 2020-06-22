58f9d427e144da5799c59e72a111ba502c041176
Gerald Rieß <g.riess@gmx.at>
2020-06-22 12:00:31 +0200

    const file

---
+++ src/Aufgabe1/Const.java
@@ -0,0 +1,13 @@
+package Aufgabe1;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.GraphWalk;
+
+import java.util.function.Function;
+import java.util.stream.DoubleStream;
+
+public class Const {
+  public static double PAUSE_WEIGHT = 2.0;
+  public static double HOSPITAL_WEIGHT = 3.0;
+
+}
